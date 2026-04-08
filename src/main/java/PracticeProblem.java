6cb3f1b5e5fdc7004e599cd2933f57499a68c0f8
Nithin Alagar <343954178@tdsb.ca>
2026-04-08 15:04:41 +0000

    Nithin.A

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,3 +1,10 @@
+/**
+	* File: Lesson 4.1: While Loops and accumulators
+	* Author: Nithin.A
+	*	Date Created: April 3, 2023
+	*	Date Last Modified: April 8, 2026
+	*/
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
@@ -22,6 +29,25 @@ public class PracticeProblem {
 
 	public static void q5() {
 		//Write question 5 code here
+	}
+	public static String seriesInclusive(int num){
+        String result = "1";
+		int i = 1;
+		while (i<num){
+		    i++;
+			result = result + " " + i;
+		}
+		return result;
+	}
+
+	public static seriesExclusive(Int num){
+		String result = "0";
+		int i = 0;
+		while (i<num-1){
+		    i++;
+			result = result + " " + i;
+		}
+		return result;
 	}
 
 }
